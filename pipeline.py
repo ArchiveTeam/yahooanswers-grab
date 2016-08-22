edb393aa86a6fd30ca3b5907462b2b62b6d506e0
Arkiver2 <Arkiver@hotmail.com>
2016-08-22 02:17:57 +0200

    initial

---
+++ pipeline.py
@@ -0,0 +1,293 @@
+# encoding=utf8
+import datetime
+from distutils.version import StrictVersion
+import hashlib
+import os.path
+import random
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import ExternalProcess
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+import shutil
+import socket
+import subprocess
+import sys
+import time
+import string
+import urllib2
+
+import seesaw
+from seesaw.externalprocess import WgetDownload
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.util import find_executable
+
+
+# check the seesaw version
+if StrictVersion(seesaw.__version__) < StrictVersion("0.8.5"):
+    raise Exception("This pipeline needs seesaw version 0.8.5 or higher.")
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_LUA will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+WGET_LUA = find_executable(
+    "Wget+Lua",
+    ["GNU Wget 1.14.lua.20130523-9a5c", "GNU Wget 1.14.lua.20160530-955376b"],
+    [
+        "./wget-lua",
+        "./wget-lua-warrior",
+        "./wget-lua-local",
+        "../wget-lua",
+        "../../wget-lua",
+        "/home/warrior/wget-lua",
+        "/usr/bin/wget-lua"
+    ]
+)
+
+if not WGET_LUA:
+    raise Exception("No usable Wget+Lua found.")
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = "20160822.01"
+USER_AGENT = 'ArchiveTeam'
+TRACKER_ID = 'yahooanswers'
+TRACKER_HOST = 'tracker.archiveteam.org'
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "CheckIP")
+        self._counter = 0
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+
+        if self._counter <= 0:
+            item.log_output('Checking IP address.')
+            ip_set = set()
+
+            ip_set.add(socket.gethostbyname('twitter.com'))
+            ip_set.add(socket.gethostbyname('facebook.com'))
+            ip_set.add(socket.gethostbyname('youtube.com'))
+            ip_set.add(socket.gethostbyname('microsoft.com'))
+            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
+            ip_set.add(socket.gethostbyname('archiveteam.org'))
+
+            if len(ip_set) != 6:
+                item.log_output('Got IP addresses: {0}'.format(ip_set))
+                item.log_output(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+                raise Exception(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+
+        # Check only occasionally
+        if self._counter <= 0:
+            self._counter = 10
+        else:
+            self._counter -= 1
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, "PrepareDirectories")
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item["item_name"]
+        escaped_item_name = item_name.replace(':', '_').replace('/', '_').replace('~', '_')
+        item_hash = hashlib.sha1(item_name).hexdigest()
+        dirname = "/".join((item["data_dir"], item_hash))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item["item_dir"] = dirname
+        item["warc_file_base"] = "%s-%s-%s" % (self.warc_prefix, item_hash,
+            time.strftime("%Y%m%d-%H%M%S"))
+
+        open("%(item_dir)s/%(warc_file_base)s.warc.gz" % item, "w").close()
+        open("%(item_dir)s/%(warc_file_base)s_data.txt" % item, "w").close()
+
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "MoveFiles")
+
+    def process(self, item):
+        # NEW for 2014! Check if wget was compiled with zlib support
+        if os.path.exists("%(item_dir)s/%(warc_file_base)s.warc" % item):
+            raise Exception('Please compile wget with zlib support!')
+
+        os.rename("%(item_dir)s/%(warc_file_base)s.warc.gz" % item,
+              "%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
+
+        os.rename("%(item_dir)s/%(warc_file_base)s_data.txt" % item,
+              "%(data_dir)s/%(warc_file_base)s_data.txt" % item)
+
+        shutil.rmtree("%(item_dir)s" % item)
+
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'yahooanswers.lua'))
+
+
+def stats_id_function(item):
+    # NEW for 2014! Some accountability hashes and stats.
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_LUA,
+            "-U", USER_AGENT,
+            "-nv",
+            "--no-cookies",
+            "--lua-script", "yahooanswers.lua",
+            "-o", ItemInterpolation("%(item_dir)s/wget.log"),
+            "--no-check-certificate",
+            "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+            "--truncate-output",
+            "-e", "robots=off",
+            "--rotate-dns",
+            "--recursive", "--level=inf",
+            "--no-parent",
+            "--page-requisites",
+            "--timeout", "30",
+            "--tries", "inf",
+            "--domains", "yahoo.com",
+            "--span-hosts",
+            "--waitretry", "30",
+            "--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+            "--warc-header", "operator: Archive Team",
+            "--warc-header", "yahooanswers-dld-script-version: " + VERSION,
+            "--warc-header", ItemInterpolation("yahooanswers-item: %(item_name)s"),
+        ]
+        
+        item_name = item['item_name']
+        assert ':' in item_name
+        item_type, item_value = item_name.split(':', 1)
+        
+        item['item_type'] = item_type
+        item['item_value'] = item_value
+        
+        assert item_type in ('10q')
+
+        languages = ['ar', 'au', 'br', 'ca', 'fr', 'de', 'in', 'id', 'it', 'malaysia', 'mx', 'nz', 'ph', 'qc', 'sg', 'tw', 'es', 'th', 'uk', 'vn', 'espanol']
+
+        if item_type == '10q':
+            questions = item_value.split(',')
+            for question in questions:
+                wget_args.extend(['--warc-header', 'yahooanswers-question: {question}'.format(**locals())])
+            for lang in languages:
+                wget_args.extend(['--warc-header', 'yahooanswers-questions-language: {lang}'.format(**locals())])
+            wget_args.extend(['https://answers.yahoo.com/question/index?qid={question}'.format(**locals()) for question in questions])
+            wget_args.extend(['https://{lang}.answers.yahoo.com/question/index?qid={question}'.format(**locals()) for lang in languages for question in questions])
+        else:
+            raise Exception('Unknown item')
+        
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+            
+        return realize(wget_args, item)
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title="yahooanswers",
+    project_html="""
+        <img class="project-logo" alt="Project logo" src="http://archiveteam.org/images/a/a3/Yahooanswers_logo.png" height="50px" title=""/>
+        <h2>answers.yahoo.com <span class="links"><a href="https://answers.yahoo.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/yahooanswers/">Leaderboard</a></span></h2>
+        <p>Archiving questions and answers from Yahoo! Answers.</p>
+    """
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker("http://%s/%s" % (TRACKER_HOST, TRACKER_ID), downloader,
+        VERSION),
+    PrepareDirectories(warc_prefix="yahooanswers"),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=2,
+        accept_on_exit_code=[0, 4, 8],
+        env={
+            "item_dir": ItemValue("item_dir"),
+            "item_value": ItemValue("item_value"),
+            "item_type": ItemValue("item_type"),
+            "warc_file_base": ItemValue("warc_file_base"),
+        }
+    ),
+    PrepareStatsForTracker(
+        defaults={"downloader": downloader, "version": VERSION},
+        file_groups={
+            "data": [
+                ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz"),
+                ItemInterpolation("%(item_dir)s/%(warc_file_base)s_data.txt")
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=4, default="1",
+        name="shared:rsync_threads", title="Rsync threads",
+        description="The maximum number of concurrent uploads."),
+        UploadWithTracker(
+            "http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz"),
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s_data.txt")
+            ],
+            rsync_target_source_path=ItemInterpolation("%(data_dir)s/"),
+            rsync_extra_args=[
+                "--recursive",
+                "--partial",
+                "--partial-dir", ".rsync-tmp",
+            ]
+            ),
+    ),
+    SendDoneToTracker(
+        tracker_url="http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue("stats")
+    )
+)
---
+++ yahooanswers.lua
@@ -0,0 +1,206 @@
+dofile("urlcode.lua")
+dofile("table_show.lua")
+
+local url_count = 0
+local tries = 0
+local item_type = os.getenv('item_type')
+local item_value = os.getenv('item_value')
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
+local downloaded = {}
+local addedtolist = {}
+
+local questions = {}
+local newquestions = {}
+local newusers = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+for question in string.gmatch(item_value, "([^,]+)") do
+  questions[question] = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url)
+  if string.match(url, "qid=[a-z0-9A-Z]+") then
+    newquestions[string.match(url, "qid=([a-z0-9A-Z]+)")] = true
+  elseif string.match(url, "show=[a-z0-9A-Z]+") then
+    newusers[string.match(url, "show=([a-z0-9A-Z]+)")] = true
+  end
+  if string.match(url, "^https?://[^/]*yimg%.com/hd/answers") then
+    return true
+  end
+  for s in string.gmatch(url, "([a-z0-9A-Z]+)") do
+    if questions[s] == true and string.match(url, "^https?://[^/]*answers%.yahoo%.com") then
+      return true
+    end
+  end
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if (downloaded[url] ~= true and addedtolist[url] ~= true)
+      and (allowed(url) or html == 0)
+      and not string.match(url, "https?://[^/]*answers%.yahoo%.com/question/index%?qid=[0-9a-zA-Z]+&a?m?p?;?page=[0-9]+") then
+    addedtolist[url] = true
+    return true
+  else
+    return false
+  end
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+
+  downloaded[url] = true
+  
+  local function check(urla)
+    local url = string.match(urla, "^([^#]+)")
+    if (downloaded[url] ~= true and addedtolist[url] ~= true) and allowed(url) then
+      if string.match(url, "&amp;") then
+        table.insert(urls, { url=string.gsub(url, "&amp;", "&") })
+        addedtolist[url] = true
+        addedtolist[string.gsub(url, "&amp;", "&")] = true
+      else
+        table.insert(urls, { url=url })
+        addedtolist[url] = true
+      end
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      if not string.match(newurl, "^https?://[^/]*answers%.yahoo%.com/question/index%?qid=[0-9a-zA-Z]+&a?m?p?;?page=[0-9]+") then
+        check(newurl)
+      end
+    elseif string.match(newurl, "^https?:\\/\\/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      check(string.match(url, "^(https?:)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(string.match(url, "^(https?:)")..newurl)
+    elseif string.match(newurl, "^\\/") then
+      check(string.match(url, "^(https?://[^/]+)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(string.match(url, "^(https?://[^/]+)")..newurl)
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(string.match(url, "^(https?://[^%?]+)")..newurl)
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?") or string.match(newurl, "^[/\\]") or string.match(newurl, "^[jJ]ava[sS]cript:") or string.match(newurl, "^[mM]ail[tT]o:") or string.match(newurl, "^%${")) then
+      check(string.match(url, "^(https?://.+/)")..newurl)
+    end
+  end
+  
+  if allowed(url) then
+    html = read_file(file)
+    for newurl in string.gmatch(html, '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, 'href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for p in string.gmatch(html, '<li[^>]+data%-ya%-type="answer"[^>]+>') do
+      if string.match(p, 'data%-ya%-answer%-id="[^"]+"')
+        and string.match(p, 'data%-ya%-question%-id="[^"]+"') then
+        local answer_id = string.match(p, 'data%-ya%-answer%-id="([^"]+)"')
+        local question_id = string.match(p, 'data%-ya%-question%-id="([^"]+)"')
+        checknewurl('/_module?name=YAAnsCommentsModule&qid=' .. question_id .. '&aid=' .. string.gsub(answer_id, "=", "%%3D"))
+      end
+    end
+  end
+
+  return urls
+end
+  
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  -- NEW for 2014: Slightly more verbose messages because people keep
+  -- complaining that it's not moving or not working
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \n")
+  io.stdout:flush()
+
+  if (status_code >= 200 and status_code <= 399) then
+    if string.match(url.url, "https://") then
+      local newurl = string.gsub(url.url, "https://", "http://")
+      downloaded[newurl] = true
+    else
+      downloaded[url.url] = true
+    end
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 400 and status_code ~= 404) or
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    os.execute("sleep 1")
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if allowed(url["url"]) then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local file = io.open(item_dir..'/'..warc_file_base..'_data.txt', 'w')
+  for newquestion, _ in pairs(newquestions) do
+    file:write("question:" .. newquestion .. "\n")
+  end
+  for newuser, _ in pairs(newusers) do
+    file:write("user:" .. newuser .. "\n")
+  end
+  file:close()
+end
